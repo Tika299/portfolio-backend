fde21cff849cddfd054fc8aef789a26758c97305
Tika299 <vukofa9120@gmail.com>
2026-03-28 19:04:55 +0700

    commit endpoint health check

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+// routes/web.php
+Route::get('/health', function () {
+    return response()->json(['status' => 'ok', 'time' => now()]);
+});

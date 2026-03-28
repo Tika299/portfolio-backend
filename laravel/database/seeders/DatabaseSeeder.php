62deaddae1e5b481fc15253d0738046c3b3b5290
Tika299 <vukofa9120@gmail.com>
2026-03-28 15:24:07 +0700

    update seeder database

--- laravel/database/seeders/DatabaseSeeder.php
+++ laravel/database/seeders/DatabaseSeeder.php
@@ -17,11 +17,11 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // Kiểm tra nếu chưa có user này thì mới tạo
-        if (!User::where('email', 'admin@example.com')->exists()) {
+        if (!User::where('email', 'vukofa9120@gmail.com')->exists()) {
             User::create([
                 'name' => 'Admin Vũ',
-                'email' => 'admin@example.com', // Thay bằng email của bạn
-                'password' => Hash::make('12345678'), // Thay bằng mật khẩu của bạn
+                'email' => 'vukofa9120@gmail.com', // Thay bằng email của bạn
+                'password' => Hash::make('290905'), // Thay bằng mật khẩu của bạn
             ]);
         }
     }

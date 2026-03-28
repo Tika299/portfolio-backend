f11c0131d6d841efd4dc7d095dff91c5ee4b29c7
Tika299 <vukofa9120@gmail.com>
2026-03-28 14:59:50 +0700

    Fix pgsql driver and storage permissions

--- laravel/docker-entrypoint.sh
+++ laravel/docker-entrypoint.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+#!/bin/bash
+
+# Tạo file log nếu chưa có và cấp quyền ngay lập tức
+touch /var/www/html/storage/logs/laravel.log
+chown www-data:www-data /var/www/html/storage/logs/laravel.log
+chmod 664 /var/www/html/storage/logs/laravel.log
+
 # 1. Chờ một chút để chắc chắn Database đã sẵn sàng (khoảng 5 giây)
 sleep 5
 
 # 2. Chạy lệnh Migration (bắt buộc có --force vì đây là môi trường Production)
 echo "Running migrations..."
 php artisan migrate --force
+
+echo "Starting Apache..."
+exec apache2-foreground
 
 # 3. Tạo liên kết Storage (để hiện ảnh)
 echo "Linking storage..."

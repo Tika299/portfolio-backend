7e2ab6758c756675c535451fc2c51b8409380293
Tika299 <vukofa9120@gmail.com>
2026-04-02 19:24:15 +0700

    Revert "Fix nested cloud key in cloudinary config"
    This reverts commit 009024def5ee753da3ff6505545d1da32d5dc911.

--- laravel/config/cloudinary.php
+++ laravel/config/cloudinary.php
@@ -6,13 +6,7 @@ return [
     | Cloudinary Configuration
     |--------------------------------------------------------------------------
     */
-
-    // Đây chính là cái "key" mà hệ thống đang báo thiếu
-    'cloud' => [
-        'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
-        'api_key'    => env('CLOUDINARY_API_KEY'),
-        'api_secret' => env('CLOUDINARY_API_SECRET'),
-    ],
-
     'cloud_url' => env('CLOUDINARY_URL'),
+
+    'upload_preset' => env('CLOUDINARY_UPLOAD_PRESET'),
 ];

4a3cfd4e8c1efc63a6822836606e4d97e9908daf
Tika299 <vukofa9120@gmail.com>
2026-04-05 09:03:31 +0700

    commit update postresource

--- laravel/app/Http/Resources/PostResource.php
+++ laravel/app/Http/Resources/PostResource.php
@@ -10,11 +10,11 @@ class PostResource extends JsonResource
     // Nếu có logic riêng cho Post thì thêm vào đây, nếu không thì kế thừa toàn bộ từ ProjectResource
     public function toArray(Request $request): array
     {
-        $thumbnail = $this->thumbnail;
+        $cover_image = $this->cover_image;
 
         // Tự nối link chuẩn của Supabase
-        if ($thumbnail && !str_starts_with($thumbnail, 'http')) {
-            $thumbnail = "https://lpctoylpxvdqwjqcxeoy.storage.supabase.co/storage/v1/object/public/portfolio/" . $thumbnail;
+        if ($cover_image && !str_starts_with($cover_image, 'http')) {
+            $cover_image = "https://lpctoylpxvdqwjqcxeoy.storage.supabase.co/storage/v1/object/public/portfolio/" . $cover_image;
         }
         return [
             'id' => $this->id,
@@ -23,9 +23,11 @@ class PostResource extends JsonResource
             'summary' => $this->summary,
             'content' => $this->content,
             // Sử dụng logic link sạch giống như Project
-            'thumbnail' => $thumbnail,
-            'published_at' => $this->published_at ? $this->published_at->format('d/m/Y') : null,
+            'cover_image' => $cover_image,
+            'views' => $this->views,
             'is_published' => $this->is_published,
+            'published_at' => $this->published_at ? $this->published_at->format('d/m/Y') : null,
+            'created_at' => $this->created_at ? $this->created_at->format('d/m/Y') : null,
         ];
     }
 }
